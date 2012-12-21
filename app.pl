cc3963a39ecf006839dcddfd15f0c2721078ab13
Kang-min Liu <gugod@gugod.org>
2012-12-21 01:40:26 +0100

    the app!

---
+++ app.pl
@@ -0,0 +1,23 @@
+use v5.10;
+use Mojolicious::Lite;
+
+use Data::HanConvert::cn2tw;
+use Data::HanConvert::cn2tw_characters;
+use Convert::Moji;
+
+helper s2t => sub {
+    state $s2t_converter = Convert::Moji->new([ table => { %$Data::HanConvert::cn2tw, %$Data::HanConvert::cn2tw_characters } ]);
+    my $self = shift;
+    my $text = shift;
+
+    return $s2t_converter->convert($text);
+};
+
+
+get '/s2t' => sub {
+    my $self = shift;
+    my $text = $self->param("t");
+    $self->render_text(text => $self->s2t($text));
+};
+
+app->start;
